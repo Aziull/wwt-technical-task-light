538f6701fddf2fc7a8a3fed4a7946da11e4dbbc8
Aziul <y.nazar9@gmail.com>
2024-09-19 17:09:10 +0300

    created adaptive layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,42 @@
-import { Box } from '@chakra-ui/react'
+import React from 'react'
 
-export const App = () => {
+import { Box, Show } from '@chakra-ui/react'
+
+import { TaskForm } from '@components/forms/TaskForm'
+import { TaskGrid } from '@components/layout/TaskGrid'
+import { TaskTabs } from '@components/layout/TaskTabs'
+import { Task } from '@store/types'
+import { useTaskStore } from '@store/useTaskStore'
+
+export const App: React.FC = () => {
+	const { tasks, addTask } = useTaskStore()
+
+	const handleSaveTask = (task: Task) => {
+		addTask(task)
+	}
+
 	return (
 		<Box
-			maxW="90rem"
+			maxW="80vw"
 			mx="auto"
-			minH="100dvh"
-		></Box>
+			minH="100vh"
+			pt="60px"
+		>
+			<Box
+				w={{ base: '100%', md: '66%' }}
+				mx="auto"
+			>
+				<TaskForm
+					task={null}
+					onSave={handleSaveTask}
+				/>
+			</Box>
+			<Show below="md">
+				<TaskTabs tasks={tasks} />
+			</Show>
+			<Show above="md">
+				<TaskGrid tasks={tasks} />
+			</Show>
+		</Box>
 	)
 }
